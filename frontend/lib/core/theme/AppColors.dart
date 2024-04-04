1d76eb65b83d30da7035cba2820ed0704c31deee
Nathan M. Shiferaw <natms24@gmail.com>
2024-04-04 10:35:24 +0300

    Added app color palettes

---
+++ frontend/lib/core/theme/AppColors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+  static const blackLighter = Color(0xFF333333);
+
+  static const listener1 = Color.fromRGBO(57, 220, 243, 1);
+  // static const listener2 = Color.fromRGBO(57, 220, 243, 1);
+  // static const listner3 = Color.fromRGBO(57, 220, 243, 1);
+
+  static const artist1 = Color.fromRGBO(237, 129, 255, 1);
+  // static const artist2 = Color.fromRGBO(237, 129, 255, 1);
+  // static const artist3 = Color.fromRGBO(237, 129, 255, 1);
+
+  static const green = Color.fromARGB(255, 62, 185, 66);
+  static const yellow = Color.fromARGB(255, 246, 205, 70);
+  static const red = Color.fromARGB(255, 229, 51, 48);
+
+  static const fontColor = Color(0xF3F3F3FF);
+}
